9637dba94d00dee29cdd41824b1495db7856e16a
TresorKL <trezmathieu@gmail.com>
2022-08-07 19:24:41 +0200

    action/tool bar done

--- app/src/main/java/com/example/twitterclone/MainActivity.java
+++ app/src/main/java/com/example/twitterclone/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.twitterclone;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.google.android.material.navigation.NavigationView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    CircleImageView menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        menu = findViewById(R.id.user);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                drawerLayout.openDrawer(GravityCompat.START);
+
+            }
+        });
+
+
     }
 }
