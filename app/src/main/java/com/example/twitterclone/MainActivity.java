90a4368474be418b368c75119b52339ecf0e6ace
TresorKL <trezmathieu@gmail.com>
2022-08-08 14:25:07 +0200

    style bottom nav bar

--- app/src/main/java/com/example/twitterclone/MainActivity.java
+++ app/src/main/java/com/example/twitterclone/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.twitterclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -7,15 +8,18 @@ import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.core.view.WindowCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -24,7 +28,8 @@ public class MainActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
-    CircleImageView menu;
+    BottomNavigationView bottomNavigationView;
+    //CircleImageView menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,16 +51,49 @@ public class MainActivity extends AppCompatActivity {
         window.setStatusBarColor(ContextCompat.getColor(MainActivity.this, R.color.black));
 
 
-        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
-        menu = findViewById(R.id.user);
-        menu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                drawerLayout.openDrawer(GravityCompat.START);
+        HomeFragment homeFragment = new HomeFragment();
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction()
+                .replace(R.id.mainFragment, homeFragment, homeFragment.getTag())
+                .commit();
 
-            }
-        });
+
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.home:
+                                //need change icon of favotites here.
+                            case R.id.search:
+
+                            case R.id.space:
+
+                            case R.id.notif:
+
+                            case R.id.message:
+
+                        }
+                        return true;
+                    }
+                });
+
+
+
+//        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+//        menu = findViewById(R.id.user);
+//        menu.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                drawerLayout.openDrawer(GravityCompat.START);
+//
+//            }
+//        });
 
 
     }
