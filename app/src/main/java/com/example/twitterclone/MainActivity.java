ff8c492a9db334daaa156bb7149229b8c4b49945
TresorKL <trezmathieu@gmail.com>
2022-08-08 12:42:59 +0200

    customise status bar

--- app/src/main/java/com/example/twitterclone/MainActivity.java
+++ app/src/main/java/com/example/twitterclone/MainActivity.java
@@ -3,13 +3,18 @@ package com.example.twitterclone;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
+import androidx.core.view.WindowCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -25,6 +30,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//------------------------------------------------------------
+        // customise status bar
+//------------------------------------------------------------
+        Window window = MainActivity.this.getWindow();
+
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+        // finally change the color
+        window.setStatusBarColor(ContextCompat.getColor(MainActivity.this, R.color.black));
 
 
         DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
